583642db1e47cee7f147112993ebeef451aa3297
GelidCrow <c.mastronardo@studenti.uniba.it>
2016-06-10 13:10:07 +0200

    a
    a

--- cltree.py
+++ cltree.py
@@ -15,58 +15,66 @@ import random
 from logr import logr
 from utils import check_is_fitted
 
+
 ###############################################################################
 
 @numba.njit
-def cMI_numba(n_features, 
-              log_probs, 
-              log_j_probs, 
+def cMI_numba(n_features,
+              log_probs,
+              log_j_probs,
               MI):
     for i in range(n_features):
-        for j in range(i+1,n_features):
+        for j in range(i + 1, n_features):
             for v0 in range(2):
                 for v1 in range(2):
-                    MI[i,j] = MI[i,j] + np.exp(log_j_probs[i,j,v0,v1])*( log_j_probs[i,j,v0,v1] - log_probs[i,v0] - log_probs[j,v1])
-                    MI[j,i] = MI[i,j]
+                    MI[i, j] = MI[i, j] + np.exp(log_j_probs[i, j, v0, v1]) * (
+                    log_j_probs[i, j, v0, v1] - log_probs[i, v0] - log_probs[j, v1])
+                    MI[j, i] = MI[i, j]
     return MI
 
+
 @numba.njit
-def log_probs_numba(n_features, 
-                    scope, 
-                    n_samples, 
-                    alpha, 
-                    mpriors, 
-                    priors, 
-                    log_probs, 
-                    log_j_probs, 
-                    log_c_probs, 
-                    cond, 
+def log_probs_numba(n_features,
+                    scope,
+                    n_samples,
+                    alpha,
+                    mpriors,
+                    priors,
+                    log_probs,
+                    log_j_probs,
+                    log_c_probs,
+                    cond,
                     p):
     for i in range(n_features):
         id_i = scope[i]
-        prob = (p[i] + alpha*mpriors[id_i,1])/(n_samples + alpha)
-        log_probs[i,0] = logr(1-prob)
-        log_probs[i,1] = logr(prob)
+        prob = (p[i] + alpha * mpriors[id_i, 1]) / (n_samples + alpha)
+        log_probs[i, 0] = logr(1 - prob)
+        log_probs[i, 1] = logr(prob)
 
     for i in range(n_features):
         for j in range(n_features):
             if i != j:
                 id_i = scope[i]
                 id_j = scope[j]
-                log_j_probs[i,j,1,1] = cond[i,j] 
-                log_j_probs[i,j,0,1] = cond[j,j] - cond[i,j] 
-                log_j_probs[i,j,1,0] = cond[i,i] - cond[i,j] 
-                log_j_probs[i,j,0,0] = n_samples - log_j_probs[i,j,1,1] - log_j_probs[i,j,0,1] - log_j_probs[i,j,1,0]
+                log_j_probs[i, j, 1, 1] = cond[i, j]
+                log_j_probs[i, j, 0, 1] = cond[j, j] - cond[i, j]
+                log_j_probs[i, j, 1, 0] = cond[i, i] - cond[i, j]
+                log_j_probs[i, j, 0, 0] = n_samples - log_j_probs[i, j, 1, 1] - log_j_probs[i, j, 0, 1] - log_j_probs[
+                    i, j, 1, 0]
 
-                log_j_probs[i,j,1,1] = logr((log_j_probs[i,j,1,1] + alpha*priors[id_i,id_j,1,1]) / ( n_samples + alpha))
-                log_j_probs[i,j,0,1] = logr((log_j_probs[i,j,0,1] + alpha*priors[id_i,id_j,0,1]) / ( n_samples + alpha))
-                log_j_probs[i,j,1,0] = logr((log_j_probs[i,j,1,0] + alpha*priors[id_i,id_j,1,0]) / ( n_samples + alpha))
-                log_j_probs[i,j,0,0] = logr((log_j_probs[i,j,0,0] + alpha*priors[id_i,id_j,0,0]) / ( n_samples + alpha))
+                log_j_probs[i, j, 1, 1] = logr(
+                    (log_j_probs[i, j, 1, 1] + alpha * priors[id_i, id_j, 1, 1]) / (n_samples + alpha))
+                log_j_probs[i, j, 0, 1] = logr(
+                    (log_j_probs[i, j, 0, 1] + alpha * priors[id_i, id_j, 0, 1]) / (n_samples + alpha))
+                log_j_probs[i, j, 1, 0] = logr(
+                    (log_j_probs[i, j, 1, 0] + alpha * priors[id_i, id_j, 1, 0]) / (n_samples + alpha))
+                log_j_probs[i, j, 0, 0] = logr(
+                    (log_j_probs[i, j, 0, 0] + alpha * priors[id_i, id_j, 0, 0]) / (n_samples + alpha))
 
-                log_c_probs[i,j,1,1] = log_j_probs[i,j,1,1] - log_probs[j,1]
-                log_c_probs[i,j,0,1] = log_j_probs[i,j,0,1] - log_probs[j,1]
-                log_c_probs[i,j,1,0] = log_j_probs[i,j,1,0] - log_probs[j,0]
-                log_c_probs[i,j,0,0] = log_j_probs[i,j,0,0] - log_probs[j,0]
+                log_c_probs[i, j, 1, 1] = log_j_probs[i, j, 1, 1] - log_probs[j, 1]
+                log_c_probs[i, j, 0, 1] = log_j_probs[i, j, 0, 1] - log_probs[j, 1]
+                log_c_probs[i, j, 1, 0] = log_j_probs[i, j, 1, 0] - log_probs[j, 0]
+                log_c_probs[i, j, 0, 0] = log_j_probs[i, j, 0, 0] - log_probs[j, 0]
 
     return (log_probs, log_j_probs, log_c_probs)
 
@@ -77,9 +85,8 @@ def compute_log_factors(tree,
                         log_probs,
                         log_c_probs,
                         log_factors):
-
-    for feature in range(0,n_features):
-        if tree[feature]==-1:
+    for feature in range(0, n_features):
+        if tree[feature] == -1:
             log_factors[feature, 0, 0] = log_probs[feature, 0]
             log_factors[feature, 0, 1] = log_probs[feature, 0]
             log_factors[feature, 1, 0] = log_probs[feature, 1]
@@ -88,14 +95,15 @@ def compute_log_factors(tree,
             parent = int(tree[feature])
             for feature_val in range(2):
                 for parent_val in range(2):
-                    log_factors[feature, feature_val, parent_val] = log_c_probs[feature, parent, feature_val, parent_val]
+                    log_factors[feature, feature_val, parent_val] = log_c_probs[
+                        feature, parent, feature_val, parent_val]
 
     return log_factors
+
 
 ###############################################################################
 
 class Cltree:
-
     def __init__(self):
         self.num_trees = 1
         self.num_edges = 0
@@ -104,7 +112,7 @@ class Cltree:
     def is_forest(self):
         return self._forest
 
-    def fit(self, X,vdata, m_priors, j_priors, alpha=1.0, sample_weight=None, scope=None, and_leaves=False,
+    def fit(self, X, vdata, m_priors, j_priors, alpha=1.0, sample_weight=None, scope=None, and_leaves=False,
             forest_approach=None):
         """Fit the model to the data.
 
@@ -135,7 +143,7 @@ class Cltree:
         It has at least one element which defines the name of the approach to be used,
         the others parameters are strictly dependent on the approach chosen.
             -ii => Iterative Improvement
-            -vns => Variable Neighbourhood Search
+            -rii => Randomised Iterative Improvement
 
         """
 
@@ -153,9 +161,7 @@ class Cltree:
         else:
             self.n_samples = np.sum(sample_weight)
 
-
         (log_probs, log_j_probs, log_c_probs) = self.compute_log_probs(X, sample_weight, m_priors, j_priors)
-
 
         MI = self.cMI(log_probs, log_j_probs)
         " the tree is represented as a sequence of parents"
@@ -166,29 +172,24 @@ class Cltree:
         self.tree = np.zeros(self.n_features, dtype=np.int)
         self.tree[0] = -1
         for p in range(1, self.n_features):
-            self.tree[p]=dfs_tree[1][p]
+            self.tree[p] = dfs_tree[1][p]
 
         # computing the factored represetation
         self.log_factors = np.zeros((self.n_features, 2, 2))
         self.log_factors = compute_log_factors(self.tree, self.n_features, log_probs, log_c_probs, self.log_factors)
 
-        self.current_best_validationll=self.score_samples_log_proba(vdata)
-
+        self.current_best_validationll = self.score_samples_log_proba(vdata)
 
         if self.and_leaves:
-            self.__makeForest(vdata, log_probs, log_c_probs,forest_approach)
-
-
-
+            self.__makeForest(vdata, log_probs, log_c_probs, forest_approach)
 
         self.num_edges = self.n_features - self.num_trees
 
-
     def compute_log_probs(self, X, sample_weight, m_priors, j_priors):
         """ WRITEME """
-        log_probs = np.zeros((self.n_features,2))
-        log_c_probs = np.zeros((self.n_features,self.n_features,2,2))
-        log_j_probs = np.zeros((self.n_features,self.n_features,2,2))
+        log_probs = np.zeros((self.n_features, 2))
+        log_c_probs = np.zeros((self.n_features, self.n_features, 2, 2))
+        log_j_probs = np.zeros((self.n_features, self.n_features, 2, 2))
 
         sparse_cooccurences = sparse.csr_matrix(X)
 
@@ -198,18 +199,18 @@ class Cltree:
         else:
             weighted_X = np.einsum('ij,i->ij', X, sample_weight)
             cooccurences = sparse_cooccurences.T.dot(weighted_X)
-        p = cooccurences.diagonal() 
+        p = cooccurences.diagonal()
 
-        return log_probs_numba(self.n_features, 
-                               self.scope, 
-                               self.n_samples, 
-                               self.alpha, 
-                               m_priors, 
-                               j_priors, 
-                               log_probs, 
-                               log_j_probs, 
-                               log_c_probs, 
-                               cooccurences, 
+        return log_probs_numba(self.n_features,
+                               self.scope,
+                               self.n_samples,
+                               self.alpha,
+                               m_priors,
+                               j_priors,
+                               log_probs,
+                               log_j_probs,
+                               log_c_probs,
+                               cooccurences,
                                p)
 
     def cMI(self, log_probs, log_j_probs):
@@ -217,17 +218,17 @@ class Cltree:
         MI = np.zeros((self.n_features, self.n_features))
         return cMI_numba(self.n_features, log_probs, log_j_probs, MI)
 
-    def score_samples_log_proba(self, X, sample_weight = None):
+    def score_samples_log_proba(self, X, sample_weight=None):
         """ WRITEME """
         check_is_fitted(self, "tree")
 
-        Prob = X[:,0]*0.0
-        for feature in range(0,self.n_features):
+        Prob = X[:, 0] * 0.0
+        for feature in range(0, self.n_features):
             parent = self.tree[feature]
-            if parent ==-1:
-                Prob = Prob + self.log_factors[feature, X[:,feature],0]
+            if parent <= -1:
+                Prob = Prob + self.log_factors[feature, X[:, feature], 0]
             else:
-                Prob = Prob + self.log_factors[feature, X[:,feature], X[:,parent]]
+                Prob = Prob + self.log_factors[feature, X[:, feature], X[:, parent]]
 
         if sample_weight is None:
             m = Prob.mean()
@@ -239,27 +240,26 @@ class Cltree:
     def score_sample_log_proba(self, x):
         """ WRITEME """
         prob = 0.0
-        for feature in range(0,self.n_features):
+        for feature in range(0, self.n_features):
             parent = self.tree[feature]
-            if parent ==-1:
+            if parent <= -1:
                 prob = prob + self.log_factors[feature, x[feature], 0]
             else:
                 prob = prob + self.log_factors[feature, x[feature], x[parent]]
         return prob
-
 
     def score_samples_scope_log_proba(self, X, features, sample_weight=None):
         """
         In case of a forest, this procedure compute the ll of a single tree of the forest.
         The features parameter is the list of the features of the corresponding tree.
         """
-        Prob = X[:,0]*0.0
+        Prob = X[:, 0] * 0.0
         for feature in features:
             parent = self.tree[feature]
-            if parent ==-1:
-                Prob = Prob + self.log_factors[feature, X[:,feature], 0]
+            if parent <= -1:
+                Prob = Prob + self.log_factors[feature, X[:, feature], 0]
             else:
-                Prob = Prob + self.log_factors[feature, X[:,feature], X[:,parent]]
+                Prob = Prob + self.log_factors[feature, X[:, feature], X[:, parent]]
 
         if sample_weight is None:
             m = Prob.mean()
@@ -274,106 +274,96 @@ class Cltree:
         prob = 0.0
         for feature in features:
             parent = self.tree[feature]
-            if parent==-1:
+            if parent == -1:
                 prob = prob + self.log_factors[feature, x[feature], 0]
             else:
                 prob = prob + self.log_factors[feature, x[feature], x[parent]]
         return prob
-    def score_samples_log_proba_v(self, X,tree,log_probs,log_c_probs):
+
+    def score_samples_log_proba_v(self, X, tree, log_probs, log_c_probs):
         """ WRITEME """
-        prob = X[:,0]*0.0
+        prob = X[:, 0] * 0.0
         log_factors = np.zeros((self.n_features, 2, 2))
-        log_factors=compute_log_factors(tree, self.n_features, log_probs, log_c_probs, log_factors)
-        for feature in range(0,self.n_features):
+        log_factors = compute_log_factors(tree, self.n_features, log_probs, log_c_probs, log_factors)
+        for feature in range(0, self.n_features):
             parent = tree[feature]
-            if parent ==-1:
-                prob = prob + log_factors[feature, X[:,feature], 0]
+            if parent <= -1:
+                prob = prob + log_factors[feature, X[:, feature], 0]
             else:
-                prob = prob + log_factors[feature, X[:,feature], X[:,parent]]
+                prob = prob + log_factors[feature, X[:, feature], X[:, parent]]
 
         return prob.mean()
 
-    def __makeForest(self, vdata, log_probs, log_c_probs,forest_approach):
-        if forest_approach[0]=='ii':
-            self.__iterative_improvement(vdata,log_probs,log_c_probs)
-        elif forest_approach[0]=='rii':
-            p=0.7
-            t=10
-            if len(forest_approach)>1:
-                p=float(forest_approach[1])
-            if len(forest_approach)>2:
-                t=int(forest_approach[2])
-            self.__Randomised_Iterative_Improvement(vdata,log_probs,log_c_probs,probability=p,times=t)
+    def __makeForest(self, vdata, log_probs, log_c_probs, forest_approach):
+        if forest_approach[0] == 'ii':
+            self.__iterative_improvement(vdata, log_probs, log_c_probs)
+        elif forest_approach[0] == 'rii':
+            p = 0.7
+            t = 10
+            if len(forest_approach) > 1:
+                p = float(forest_approach[1])
+            if len(forest_approach) > 2:
+                t = int(forest_approach[2])
+            self.__Randomised_Iterative_Improvement(vdata, log_probs, log_c_probs, probability=p, times=t)
 
-        if self.num_trees>1:
-            self._forest=True
+        if self.num_trees > 1:
+            self._forest = True
 
-
-    def __iterative_improvement(self,vdata,log_probs,log_c_probs):
+    def __iterative_improvement(self, vdata, log_probs, log_c_probs):
         improved = True
         while improved:
-            improved=False
-            best_ll=-np.inf
-            best_edge= None
-            valid_edges=np.where(self.tree!=-1)
-            if np.size(valid_edges) >0:
+            improved = False
+            best_ll = -np.inf
+            best_edge = None
+            valid_edges = np.where(self.tree != -1)
+            if np.size(valid_edges) > 0:
                 for i in np.nditer(valid_edges):
                     new = np.copy(self.tree)
-                    new[i]= -1
-                    valid_ll = self.score_samples_log_proba_v(vdata,new,log_probs,log_c_probs)
-                    if valid_ll>best_ll:
-                        best_edge=i
-                        best_ll=valid_ll
+                    new[i] = -1
+                    valid_ll = self.score_samples_log_proba_v(vdata, new, log_probs, log_c_probs)
+                    if valid_ll > best_ll:
+                        best_edge = i
+                        best_ll = valid_ll
 
-
-                if best_ll>self.current_best_validationll:
-                    self.current_best_validationll=best_ll
-                    self.num_trees+=1
-                    self.tree[best_edge]=-1
+                if best_ll > self.current_best_validationll:
+                    self.current_best_validationll = best_ll
+                    self.num_trees += 1
+                    self.tree[best_edge] = -1
                     self.log_factors = np.zeros((self.n_features, 2, 2))
-                    self.log_factors = compute_log_factors(self.tree, self.n_features, log_probs, log_c_probs, self.log_factors)
-                    improved=True
+                    self.log_factors = compute_log_factors(self.tree, self.n_features, log_probs, log_c_probs,
+                                                           self.log_factors)
+                    improved = True
 
+    def __Randomised_Iterative_Improvement(self, vdata, log_probs, log_c_probs, probability=0.7, times=10):
+        t = 0
+        valid_edges = np.where(self.tree != -1)
+        while t < times and np.size(valid_edges) > 0:
 
+            n_ll = -np.inf
+            r = random.uniform(0, 1)
+            edge_to_cut = None
 
-
-
-
-
-    def __Randomised_Iterative_Improvement(self,vdata,log_probs,log_c_probs,probability=0.7,times=10):
-        t=0
-        valid_edges=np.where(self.tree!=-1)
-        while t<times and np.size(valid_edges)>0:
-
-            n_ll=-np.inf
-            r=random.uniform(0,1)
-            edge_to_cut=None
-
-            if r>probability: #random cut
-                r=random.randint(0,np.size(valid_edges)-1)
+            if r > probability:  # random cut
+                r = random.randint(0, np.size(valid_edges) - 1)
                 new = np.copy(self.tree)
-                new[r]=-1
-                edge_to_cut=r
-                n_ll=self.score_samples_log_proba_v(vdata,new,log_probs,log_c_probs)
-            else:#best cut , if any
+                new[r] = -1
+                edge_to_cut = r
+                n_ll = self.score_samples_log_proba_v(vdata, new, log_probs, log_c_probs)
+            else:  # best cut , if any
                 for i in np.nditer(valid_edges):
                     n = np.copy(self.tree)
-                    n[i]= -1
-                    valid_ll = self.score_samples_log_proba_v(vdata,n,log_probs,log_c_probs)
+                    n[i] = -1
+                    valid_ll = self.score_samples_log_proba_v(vdata, n, log_probs, log_c_probs)
                     if valid_ll > n_ll:
-                        n_ll=valid_ll
-                        edge_to_cut=i
+                        n_ll = valid_ll
+                        edge_to_cut = i
 
-            if n_ll>self.current_best_validationll:
-                self.current_best_validationll=n_ll
-                self.tree[edge_to_cut]=-1
-                self.num_trees+=1
+            if n_ll > self.current_best_validationll:
+                self.current_best_validationll = n_ll
+                self.tree[edge_to_cut] = -1
+                self.num_trees += 1
                 self.log_factors = np.zeros((self.n_features, 2, 2))
-                self.log_factors = compute_log_factors(self.tree, self.n_features, log_probs, log_c_probs, self.log_factors)
-            t+=1
-            valid_edges=np.where(self.tree!=-1)
-
-
-
-
-
+                self.log_factors = compute_log_factors(self.tree, self.n_features, log_probs, log_c_probs,
+                                                       self.log_factors)
+            t += 1
+            valid_edges = np.where(self.tree != -1)
